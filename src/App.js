5822718648b80017fbffd661299047081c74724b
bigbangk01 <phamthachtu196@gmail.com>
2021-04-11 21:29:44 +0700

    Home page and User page add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       
+      <div class="page-header">
+        <img className='img' src="love.png"/>
+        <h1>ChatVuiVe<small>.com</small></h1>
+      </div>
+      
+      <div class="container">
+          <button id="signIn" type="button" className="btn btn-lg btn-primary">Sign in</button>
+          
+          <button id="signUp"type="button" className="btn btn-lg btn-success">Sign up</button>
+      </div>
     </div>
   );
 }
---
+++ src/User.js
@@ -0,0 +1,22 @@
+import './User.css';
+function App() {
+  return (
+    <div className="App">
+        <section className="Form">
+            <nav class="navbar navbar-inverse">
+            <a className="navbar-brand"><img className='img' src="love.png"/>CHAT VUI VE</a>
+            <ul className="nav navbar-nav">
+                <li className="active">
+                    <a>Home</a>
+                </li>
+                <li>
+                    <a>Add new friends</a>
+                </li>
+            </ul>
+            </nav>
+        </section>
+    </div>
+  );
+}
+
+export default User;
